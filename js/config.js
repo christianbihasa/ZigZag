6c11681284831f848f746a402275fd1f0543373f
christianbihasa <christianlouie.bihasa@gmail.com>
2026-07-21 08:27:13 +0800

    add settings modal for modifications

--- js/config.js
+++ js/config.js
@@ -1,8 +1,25 @@
 export const CONFIG = {
     BLOCK_SIZE: 2,
-    INITIAL_SPEED: 0.085, 
-    ACCELERATION: 0.0002, 
     
+    SPEED_PRESETS: {
+        slow: {
+            label: 'Slow',
+            initial: 0.055,
+            accel: 0.0001
+        },
+        normal: {
+            label: 'Normal',
+            initial: 0.085,
+            accel: 0.0002
+        },
+        fast: {
+            label: 'Fast',
+            initial: 0.12,
+            accel: 0.00035
+        }
+    },
+    DEFAULT_SPEED_PRESET: 'normal',
+
     BALL_RADIUS: 0.5,
     CAMERA_DISTANCE: 10,
     CAMERA_LERP: 0.08,
